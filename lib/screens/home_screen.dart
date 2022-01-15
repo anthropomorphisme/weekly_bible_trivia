064cd9a14ec8c3b5ce70a51d3a7994f9a320ff93
anthropomorphisme <salmaneser@protonmail.com>
2022-01-15 16:57:42 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,43 +6,48 @@ import 'package:weekly_bible_trivia/containers/tab_selector.dart';
 import 'package:weekly_bible_trivia/redux/states/app_tab.dart';
 import 'package:weekly_bible_trivia/containers/active_tab.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget
+{
   const HomeScreen({Key? key}) : super(key: key);
 
 
   @override
-  HomeScreenState createState() {
+  HomeScreenState createState()
+  {
     return HomeScreenState();
   }
 }
 
-class HomeScreenState extends State<HomeScreen> {
+class HomeScreenState extends State<HomeScreen>
+{
   @override
-  void initState() {
+  void initState()
+  {
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) => ActiveTab(
-      builder: (BuildContext context, AppTab activeTab) => Scaffold(
+  Widget build(BuildContext context) => ActiveTab(builder: (BuildContext context, AppTab activeTab) => Scaffold(
           resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-              backgroundColor: Colors.white,
-            ),
+          appBar: AppBar(backgroundColor: Colors.white),
             body: _getContainer(activeTab),
             bottomNavigationBar: //TabSelector(),
-                Theme(
+            Theme(
               data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
               child: const TabSelector(),
             ),
           ));
 
-  dynamic _getContainer(AppTab activeTab) {
-    if (activeTab == AppTab.home) {
+  dynamic _getContainer(AppTab activeTab)
+  {
+    if (activeTab == AppTab.home)
+    {
       return HomeContainer();
-    } else if (activeTab == AppTab.reader) {
+    } else if (activeTab == AppTab.reader)
+    {
       return ReaderContainer();
-    } else if (activeTab == AppTab.pastTrivia) {
+    } else if (activeTab == AppTab.pastTrivia)
+    {
       return PastTriviaContainer();
     }
   }
